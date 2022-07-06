49a1c082d4b0314ebe750db69638290be92490a6
jeanrolszt <jeanrolszt@gmail.com>
2022-07-05 21:08:29 -0300

    alteração correto funcionamento do domínio cidade

--- domain.cpp
+++ domain.cpp
@@ -7,7 +7,7 @@ void Cidade::validar(string input){
     std::string cidades[]= {"Antalya","Bangkok","Delhi","Dubai","Hong Kong","Londres","Macau","Mumbai","Paris","Rio de Janeiro","Sao Paulo","Seul","Istambul","Kuala Lumpur","Nova Iorque","Osaka","Phuket","Shenzhen","Toquio"};
     bool valido = false;
     int tamanhoCidades = sizeof(cidades)/sizeof(cidades[0]);
-    for(int i=0;i>tamanhoCidades;i++){
+    for(int i=0;i<tamanhoCidades;i++){
         if(cidades[i]==input){
             valido=true;
             break;
